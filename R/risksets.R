41921bfac0f15fac93a8452b43da4a60fe159e44
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2015-11-20 14:36:15 -0500

    added comments

--- R/risksets.R
+++ R/risksets.R
@@ -13,62 +13,65 @@ b2 <- 50
 c1 <- 0.0
 c2 <- 0.0
 
+# e event type 0-censored, 1-event of interest, 2-competing event
+# t observed time/endpoint
+# z is a binary covariate
 DT <- data.table(z=rbinom(nobs, 1, 0.5))
-DT[,`:=` ("t1"=rweibull(nobs, a1, b1 * exp(z * c1)^(-1/a1)),
-          "t2"=rweibull(nobs, a2, b2 * exp(z * c2)^(-1/a2)),
+DT[,`:=` ("t_event"=rweibull(nobs, a1, b1 * exp(z * c1)^(-1/a1)),
+          "t_comp"=rweibull(nobs, a2, b2 * exp(z * c2)^(-1/a2)),
           "tlim"=10)]
-DT[,`:=`("e"=1 * (t1 < t2) + 2 * (t1 >= t2), "t"=pmin(t1,t2))]
+DT[,`:=`("e"=1 * (t_event < t_comp) + 2 * (t_event >= t_comp),
+         "t"=pmin(t_event, t_comp))]
 DT[t >= tlim, e:=0]
 DT[t >= tlim, t:=tlim]
 DT[, idx:=order(t)]
+# put the e=0 people at the bottom of the population-time plot and people with
+# short values of t at the top
 DT[ DT[["idx"]], ycoord:=(nobs:1)]
-#this line works
+# sample y coordinates for each event, so that we can see the incidence density
+# on population-time plots. Sampling from people with e=0 or 2 who have an
+# observed time t greater than that of a fixed individual who had the event
 DT[e==1, yc:=lapply(t, function(i){sample(DT[t>=i & e!=1,ycoord],1)})]
 
 
-
 # get row number of subjects who have an event==1, and either covariate value
-cond <- DT[e==1 & z %in% c(0,1),which=T]
+cond <- DT[e==1 & z %in% c(0,1), which=T]
 
 # get event time of subjects who have an event==1, and either covariate value
-etimes <- DT[e==1 & z %in% c(0,1),t]
+etimes <- DT[e==1 & z %in% c(0,1), t]
 
 
-DT[e==1, ycoordnew:=mapply(function(x,y) {DT[ycoord >= yc & ycoord < i,ycoord ]+1}  )]
-mapply
-    ycoord[DT[["t"]]>=i & DT[["e"]]!=1],1)}  )]
+# DT[e==1, ycoordnew:=mapply(function(x,y) {DT[ycoord >= yc & ycoord < i,ycoord ]+1}  )]
+# mapply
+#     ycoord[DT[["t"]]>=i & DT[["e"]]!=1],1)}  )]
+#
+# for (i in 1:nobs) {
+#     if (e[i] == 1) {
+#         # yc: for a given subject i with event=1, sample the id numbers from
+#         # subjects who have an event time (either from being censored or event
+#         # type=2) greater than subject i
+#         yc <- sample(ycoord[t >= t[i] & e != 1], 1)
+#
+#         ycoord[ycoord >= yc & ycoord < ycoord[i]] <- ycoord[ycoord >= yc & ycoord < ycoord[i]] + 1
+#         ycoord[i] <- yc
+#
+#     }
+# }
 
-for (i in 1:nobs) {
-    if (e[i] == 1) {
-        # yc: for a given subject i with event=1, sample the id numbers from subjects who have
-        # an event time (either from being censored or event type=2) greater than subject i 
-        yc <- sample(ycoord[t >= t[i] & e != 1], 1)
-        
-        ycoord[ycoord >= yc & ycoord < ycoord[i]] <- ycoord[ycoord >= yc & ycoord < ycoord[i]] + 1
-        ycoord[i] <- yc
-        
-    }
-}
-
-
-
-
-
-
-
-table(DT$e)
+DT[,table(e)]
 
 op <- par(mar=c(4.5,4.5,1,1), lwd=2)
 with(DT,plot(1,1,type='n', xlim=c(0,tlim[1]), ylim=c(1,nobs), xlab='Follow-up years', ylab='Population'))
-with(DT,segments(rep(0.0, length(z)), ycoord, t, ycoord, col='gray'))
-with(DT[z==1],segments(rep(0.0, length(z)), ycoord, t, ycoord, col='yellow'))
-with(DT[z==],points(t[e==1], ycoord[e==1], pch=20, col='blue', cex=0.5))
+with(DT,segments(rep(0.0, nobs), ycoord, t, ycoord, col='gray'))
+#with(DT[z==1],segments(rep(0.0, nobs), ycoord, t, ycoord, col='yellow'))
+#with(DT[z==0],points(t[e==1], yc[e==1], pch=20, col='blue', cex=0.5))
+with(DT,points(t[e==1], yc[e==1], pch=20, col='blue', cex=0.5))
 par(op)
 
 
 set.seed(1)
 nobs <- 5000
-# binary covariate 
+# binary covariate
 z <- rbinom(nobs, 1, 0.5)
 
 # simulated times based on binary covariate
@@ -107,12 +110,13 @@ ycoord[1:10]
 
 for (i in 1:nobs) {
     if (e[i] == 1) {
-        # yc: for a given subject i with event=1, sample the id numbers from subjects who have
-        # an event time (either from being censored or event type=2) greater than subject i 
+        # yc: for a given subject i with event=1, sample the id numbers from
+        # subjects who have an event time (either from being censored or event
+        # type=2) greater than subject i
         yc <- sample(ycoord[t >= t[i] & e != 1], 1)
         ycoord[ycoord >= yc & ycoord < ycoord[i]] <- ycoord[ycoord >= yc & ycoord < ycoord[i]] + 1
         ycoord[i] <- yc
-        
+
     }
 }
 
@@ -158,7 +162,7 @@ segments(rep(0.0, nobs)[idx], ycoord[idx], t[idx], ycoord[idx], col='gray')
 points(t[e==1], ycoord[e==1], pch=20, col='blue', cex=0.5)
 for (ti in (t[idx])[e[idx]==1]) {
     points(rep(ti, last)[t[idx] > ti], (ycoord[idx])[t[idx] > ti], pch=20, col='gray', cex=0.5)
-} 
+}
 par(op)
 dev.off()
 
--- R/sampling.R
+++ R/sampling.R
@@ -1,1 +1,66 @@
 # This is where all functions related to sampling should appear
+expit <- function(x) 1/(1 + exp(-x))
+logit <- function(p) log(p)-log(1-p)
+erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1
+
+# R function to create case-base dataset for use in fitting
+# parametric hazard functions via logistic regression
+# see Hanley and Miettinen, Int J Biostatistics 2009
+
+#' @param ds source dataset
+#' @param event.var event variable (1=event)
+#' @param t.var event (or censoring) time
+#' @param i.var intervention (tx) variable
+#' @param id.var patient identifier
+#' @param x.vars vector of names of regressor variables
+#' @param b.c.ratio (integer) ratio, size of base series : case series
+#' @param random if TRUE,
+#' @return returns dataset with b+c rows of person-moments (p.m), x.vars,
+#'   offset, and an indicator variable y (1 if p.m represents an event, 0
+#'   otherwise)
+
+sampleCaseBase <- function(ds, event.var, t.var,
+                           i.var, id.var, x.vars,
+                           b.c.ratio, random = TRUE) {
+
+    n.subjects <- length(ds[,t.var]) # no. of subjects
+    B <- sum(ds[,t.var])             # total person-time in base
+    c <- sum(ds[,event.var])          # no. of cases (events)
+    b <- b.c.ratio * c               # size of base series
+    offset <- log(B / b)            # offset so intercept = log(ID | x, t = 0 )
+
+    if (random) {
+        p <- ds[,t.var]/B
+        who <- sample(n.subjects, b, replace = TRUE, prob = p)
+        b.series <- ds[who,]
+        b.series <- b.series[,c(i.var,id.var,x.vars,t.var)]
+        b.series$y <- 0
+        b.series[,t.var] <- runif(b)*b.series[,t.var]
+        b.series$o <- offset
+    }
+
+    if (!random) {
+        d.t <- B/(b+1)
+        p.sum <- c(0) #Allocate memory first!!
+        for (i in 1:n.subjects) {
+            p.sum <- c(p.sum, p.sum[i] + ds[i,t.var])
+        }
+        every.d.t <- B*(1:b)/(b+1)
+        who <- findInterval(every.d.t, p.sum)
+        #print(who)
+        b.series <- ds[who,]
+        b.series <- b.series[, c(i.var,id.var,x.vars, t.var)]
+        b.series$y <- 0
+        b.series[,t.var] <- every.d.t - p.sum[who]
+        b.series$o <- offset
+    }
+
+    c.series <- ds[ ds[event.var]==1, ]
+    c.series <- c.series[,c(i.var, id.var, x.vars, t.var)]
+    c.series$y <- 1
+    c.series[, t.var] <- c.series[, t.var]
+    c.series$o <- offset
+    c.b.series <- rbind(c.series, b.series)
+
+    return(c.b.series)
+}
