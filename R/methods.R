a171645b6c08edd86edbdc814260f4ab95c3f7aa
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2016-02-29 16:03:56 -0500

    extend vglm class using S4 objects

--- R/methods.R
+++ R/methods.R
@@ -1,9 +1,35 @@
 # This is where all methods (e.g. print, plot) should appear
-#' @export
-#' @importMethodsFrom VGAM summary
-summary.compRisk <- function(object, ...) {
-    summary(object$model)
-}
-
 #' @import methods
 NULL
+
+#################
+# S4 objects ----
+
+#' An S4 class to store the output of fitSmoothHazard
+#'
+#' This class inherits from \code{vglm-class}.
+#'
+#' @slot originalData Data.frame containing the original data (i.e. before
+#'   case-base sampling). This is used by the \code{\link{absoluteRisk}}
+#'   function.
+#' @slot typeEvents Numeric factor which encodes the type of events being
+#'   considered (including censoring).
+#' @slot timeVar Character string giving the name of the time variable, as
+#'   appearing in \code{originalData}
+#' @slot eventVar Character string giving the name of the event variable, as
+#'   appearing in \code{originalData}
+#' @importClassesFrom VGAM vglm
+CompRisk <- setClass("CompRisk",
+                     slots = c(
+                         originalData = "data.frame",
+                         typeEvents = "numeric",
+                         timeVar = "character",
+                         eventVar = "character"),
+                     contains = "vglm",
+                     prototype = list(
+                         originalData = data.frame(),
+                         typeEvents = c(0,1),
+                         timeVar = "time",
+                         eventVar = "event"
+                     )
+)
