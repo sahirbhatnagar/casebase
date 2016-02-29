a186874db20bc6266ac28b0235e51f9108aad7c5
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2016-02-29 10:44:58 -0500

    add summary method for class compRisk

--- R/methods.R
+++ R/methods.R
@@ -1,1 +1,9 @@
 # This is where all methods (e.g. print, plot) should appear
+#' @export
+#' @importMethodsFrom VGAM summary
+summary.compRisk <- function(object, ...) {
+    summary(object$model)
+}
+
+#' @import methods
+NULL
