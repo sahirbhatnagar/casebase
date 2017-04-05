f0e9f3c728b0e5f9dde59cfe3e90e79b532aa3bb
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2017-04-04 20:48:44 -0600

    add details about our testing scheme

---
+++ update_doc.R
@@ -0,0 +1,5 @@
+# Source this script to update docs----
+rmarkdown::render("docs/intro.Rmd")
+rmarkdown::render("docs/smoothHazard.Rmd")
+rmarkdown::render("docs/popTime.Rmd")
+rmarkdown::render("docs/competingRisk.Rmd")
